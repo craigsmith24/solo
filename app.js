1a07ec14e090f22a0b8caa4892ff63481f07cf32
Craig Smith <craigsmith24@gmail.com>
2015-04-27 22:04:49 -0700

    more style changes

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ var totalCalories = 0;
 $(document).ready(function(){
   $(".entry").on("click", function(){
     var foodEntry = prompt('what have you eaten today?');
-    $("#foodName").html('Food Eaten');
     $("#foodName").append(foodEntry);
     var calories = parseInt(prompt('how many calories were in your ' + foodEntry + '?'));
-    $("#foodCalories").html('Calories');
     $("#foodCalories").append(calories);
     totalCalories+=calories;
     $("#total").html('Total Calories');
